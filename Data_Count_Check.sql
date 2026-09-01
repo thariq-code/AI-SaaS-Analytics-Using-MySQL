ce1c323587235e3e7f76876a68420b392bd8a6d2
thariq-code <thariqarsath786@gmail.com>
2026-09-01 15:15:23 +0530

    Delete Data_Count_Check.sql

--- Data_Count_Check.sql
+++
@@ -1,8 +0,0 @@
-USE ai_saas_analytics;
-
-SELECT COUNT(*) AS customers FROM customers;
-SELECT COUNT(*) AS plans FROM plans;
-SELECT COUNT(*) AS subscriptions FROM subscriptions;
-SELECT COUNT(*) AS payments FROM payments;
-SELECT COUNT(*) AS ai_usage FROM ai_usage;
-SELECT COUNT(*) AS support_tickets FROM support_tickets;
